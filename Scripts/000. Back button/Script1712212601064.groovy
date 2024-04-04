a8c21ad78120701d89bfb52d1262d9cd572a6440
Nextsix <Nextsix@LAPTOP-ED4PU2U8>
2024-04-04 15:59:44 +0800

    123

---
+++ Scripts/000. Back button/Script1712212601064.groovy
@@ -0,0 +1,64 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\Nextsix\\git\\katalon-agentapp\\userMapTesterV1Leo.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/000.Back Button/android.widget.TextView - Lets Get Started'), 0)
+
+Mobile.tap(findTestObject('Object Repository/000.Back Button/android.widget.TextView - Skip'), 0)
+
+'Click Search bar in Homepage (Buy)'
+Mobile.tap(findTestObject('Object Repository/000.Back Button/android.widget.TextView -'), 0)
+
+Mobile.pressBack()
+
+Mobile.tap(findTestObject('Object Repository/000.Back Button/android.view.ViewGroup'), 0)
+
+Mobile.tap(findTestObject('Object Repository/000.Back Button/android.widget.TextView - Rent'), 0)
+
+'Click Search bar in Homepage (Rent)'
+Mobile.tap(findTestObject(''), 0)
+
+Mobile.pressBack()
+
+'Click Type'
+Mobile.tap(findTestObject('Object Repository/000.Back Button/android.widget.TextView - Type'), 0)
+
+Mobile.verifyElementExist(findTestObject('Object Repository/000.Back Button/android.widget.TextView - Any Residential'), 
+    0)
+
+Mobile.pressBack()
+
+Mobile.verifyElementExist(findTestObject('Object Repository/000.Back Button/android.widget.TextView - Property Type'), 0)
+
+Mobile.pressBack()
+
+'Click Price'
+Mobile.tap(findTestObject('Object Repository/000.Back Button/android.widget.TextView - Price'), 0)
+
+Mobile.pressBack()
+
+Mobile.verifyElementExist(findTestObject(''), 0)
+
+Mobile.pressBack()
+
+Mobile.tap(findTestObject('Object Repository/000.Back Button/android.widget.TextView - Get Agent'), 0)
+
+Mobile.closeApplication()
+

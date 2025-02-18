5f1a45a177a0fe964d6725a8c33898603156d451
Nextsix <Nextsix@LAPTOP-ED4PU2U8>
2025-02-18 14:33:29 +0800

    sd

--- Test Suites/regression.ts
+++ Test Suites/regression.ts
@@ -11,11 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>df59c29e-9fb9-4138-8f02-44bbef4a2522</testSuiteGuid>
-   <testCaseLink>
-      <guid>19719b37-6671-41c2-a5fd-abb0681d5c54</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/000. Back button</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
